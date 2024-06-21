171bb1658ff23069a1bfa48e1d9df1f92640c4ed
jessicaryan19 <94927324+jessicaryan19@users.noreply.github.com>
2024-06-22 05:51:57 +0700

    Update Data_and_Preprocessing.py

--- pages/Data_and_Preprocessing.py
+++ pages/Data_and_Preprocessing.py
@@ -7,7 +7,9 @@ st.title('Data and Preprocessing')
 
 st.subheader('Dataset')
 st.markdown(
-  '''Dataset yang digunakan diambil dari Kaggle untuk mengklasifikasikan apakah suatu teks merupakan hate-speech atau non-hate-speech. Link dataset: https://www.kaggle.com/datasets/waalbannyantudre/hate-speech-detection-curated-dataset?select=HateSpeechDataset.csv'''
+  '''Dataset yang digunakan diambil dari Kaggle untuk mengklasifikasikan apakah suatu teks merupakan hate-speech atau non-hate-speech.
+  
+  Link dataset: https://www.kaggle.com/datasets/waalbannyantudre/hate-speech-detection-curated-dataset?select=HateSpeechDataset.csv'''
 )
 st.image('dataset_1.png')
 st.image('dataset_2.png')
@@ -15,3 +17,12 @@ st.image('dataset_2.png')
 # st.components.v1.iframe("https://www.kaggle.com/datasets/waalbannyantudre/hate-speech-detection-curated-dataset?select=HateSpeechDataset.csv", height=400, scrolling=True)
 
 st.subheader('Preprocessing')
+st.markdown(
+  '''Tahapan preprocessing yang dilakukan berupa:
+  1. Compress: mengkompres tiga atau lebih karakter yang berulang dengan menggunakan regular expression :blue-background[re.sub].
+  2. Tokenization: memecah kalimat menjadi kata terpisah dengan menggunakan :blue-background[word_tokenize] dari :blue-background[nltk].
+  3. Lowercasing: mengubah semua kata menjadi huruf kecil.
+  4. Stopwords and Punctuation: menghilangkan stopwords yang sering digunakan dalam bahasa tertentu dan simbol-simbol atau tanda baca.5. Alphabetical: menghilangkan karakter non alfabet.
+  6. Stemming: menggunakan :blue-background[PorterStemmer] untuk mengubah kata yang ada ke bentuk dasar sehingga mengurangi variasi.
+  7. Lemmatization: menggunakan :blue-background[WordNetLemmatizer] untuk mengubah kata menjadi bentuk kamusnya.'''
+)
