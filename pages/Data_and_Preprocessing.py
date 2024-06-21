301a089c1ae9316ad05d5d9688b964439dbf0177
jessicaryan19 <94927324+jessicaryan19@users.noreply.github.com>
2024-06-22 05:35:39 +0700

    Update Data_and_Preprocessing.py

--- pages/Data_and_Preprocessing.py
+++ pages/Data_and_Preprocessing.py
@@ -9,8 +9,8 @@ st.subheader('Dataset')
 st.markdown(
   '''Dataset yang digunakan diambil dari Kaggle untuk mengklasifikasikan apakah suatu teks merupakan hate-speech atau non-hate-speech. Link dataset: https://www.kaggle.com/datasets/waalbannyantudre/hate-speech-detection-curated-dataset?select=HateSpeechDataset.csv'''
 )
-st.image('/image/dataset_1.png')
-st.image('/image/dataset_2.png')
+st.image('../image/dataset_1.png')
+st.image('../image/dataset_2.png')
 
 
 # st.components.v1.iframe("https://www.kaggle.com/datasets/waalbannyantudre/hate-speech-detection-curated-dataset?select=HateSpeechDataset.csv", height=400, scrolling=True)
