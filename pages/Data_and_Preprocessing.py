5b8b77e73e50a0e4ae082282f7cb35dc98ba3ee9
jessicaryan19 <94927324+jessicaryan19@users.noreply.github.com>
2024-06-22 05:27:39 +0700

    Update Data_and_Preprocessing.py

--- pages/Data_and_Preprocessing.py
+++ pages/Data_and_Preprocessing.py
@@ -6,5 +6,6 @@ st.set_page_config(
 st.title('Data and Preprocessing')
 
 st.subheader('Dataset')
-st.components.v1.iframe('https://www.kaggle.com/datasets/waalbannyantudre/hate-speech-detection-curated-dataset?select=HateSpeechDataset.csv')
+st.components.v1.iframe("https://www.kaggle.com/datasets/waalbannyantudre/hate-speech-detection-curated-dataset?select=HateSpeechDataset.csv", height=400, scrolling=True)
+
 st.subheader('Preprocessing')
