5ee317ad0ac280d4580fee164c74daf9c1ae44f2
Dep <109551168+dave-andrew@users.noreply.github.com>
2024-06-22 02:20:47 +0700

    fix: prediction

--- streamlit.py
+++ streamlit.py
@@ -61,4 +61,4 @@ if st.button('Predict Hate Speech'):
     prediction = model.classify(FreqDist(paragraph))
     
     # display the prediction
-    st.write(f'The paragraph is a {prediction[0]}')
+    st.write(f'The paragraph is a {prediction}')
