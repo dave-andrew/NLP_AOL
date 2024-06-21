1b04154e7745cad64354fe38349d4c69abed5e7a
jessicaryan19 <94927324+jessicaryan19@users.noreply.github.com>
2024-06-22 04:54:48 +0700

    Test markdown

--- pages/Training_Model.py
+++ pages/Training_Model.py
@@ -5,10 +5,21 @@ st.set_page_config(
 )
 st.title('Models')
 st.subheader('Decision Tree')
-st.markdown()
+st.markdown(
+  '''Decision Tree adalah algoritma klasifikasi yang bekerja dengan cara membagi data menjadi subset-subset berdasarkan aturan tertentu, menghasilkan struktur pohon yang bercabang.
+
+  Cara Kerja:
+  1. Pemilihan Atribut: Memilih atribut yang paling informatif untuk membagi data.
+  2. Pembuatan Pohon: Membangun struktur pohon dengan membagi data secara berulang berdasarkan atribut yang dipilih.
+  3. Klasifikasi Teks Baru: Mengikuti cabang-cabang pohon berdasarkan nilai atribut teks baru untuk mencapai daun dan menggunakan label daun sebagai prediksi kelas.'''
+)
 
 st.subheader('Linear Regression')
 
 st.subheader('Naive Bayes')
 
 st.subheader('Linear SVC')
+
+st.subheader('ADA Boost')
+
+st.subheader('Random Forest')
