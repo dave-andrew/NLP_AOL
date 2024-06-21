9d19954587d995d17654f4c1ec64657b465ae265
jessicaryan19 <94927324+jessicaryan19@users.noreply.github.com>
2024-06-22 04:31:07 +0700

    Page test

---
+++ pages/Models.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Models",
+)
+st.title('Models')
+st.subheader('Decision Tree')
+
+st.subheader('Linear Regression')
+
+st.subheader('Naive Bayes')
+
+st.subheader('Linear SVC')
