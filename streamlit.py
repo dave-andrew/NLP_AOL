d5b046b3cafe7ce09a64a1e5d8a559910c1d1a5c
Dep <daveandrewnathaniel48@gmail.com>
2024-07-05 15:54:49 +0700

    fdsa

--- streamlit.py
+++ streamlit.py
@@ -5,10 +5,10 @@ import string
 import pandas as pd
 import nltk
 
-nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('wordnet')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('wordnet')
 
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
